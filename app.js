9062e1672655c97bdb3dbb33740fe4865b43b200
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-08-13 12:24:20 +0500

    update3

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 function login() {
     // email address in sign-in-form
     var input2 = document.getElementById("input2");
+    if (input2.value.length === 0) {
+        swal("Please Enter Your Email Address")
+    }
     input2.value = " ";
 
     // password in sign-in form
     var input3 = document.getElementById("input3");
+    if (input3.value.length === 0) {
+        swal("Please Enter Your Password");
+    }
+    else if (input3.value.length < 8) {
+        swal("Password Should Be Must 8 Chracter");
+    }
     input3.value = " ";
 }
 
@@ -58,52 +67,68 @@ function icon1() {
 
 
 //signup-form
-function icon4(){
-       var input3 = document.getElementById("input3");
-       input3.setAttribute("type", "text");
-   
-       var icon2 = document.getElementById("icon2");
-       icon2.style.display = "none";
-   
-       var icon1 = document.getElementById("icon1");
-       icon1.style.display = "block";
+function icon4() {
+    var input7 = document.getElementById("input7");
+    input7.setAttribute("type", "text");
 
-       var button2 = document.getElementById("button2");
-       button2.style.marginTop = "-70px";
+    var icon2 = document.getElementById("icon2");
+    icon2.style.display = "none";
 
-       var heading = document.getElementById("heading");
-       heading.style.marginTop = "-27px"
+    var icon1 = document.getElementById("icon1");
+    icon1.style.display = "block";
+
+    var button2 = document.getElementById("button2");
+    button2.style.marginTop = "-70px";
+
+    var heading = document.getElementById("heading");
+    heading.style.marginTop = "-27px"
 }
 
 function icon3() {
-      var input3 = document.getElementById("input3");
-      input3.setAttribute("type", "password");
-  
-      var icon2 = document.getElementById("icon2");
-      icon2.style.display = "block";
-      icon2.style.marginLeft = "320px"
-      icon2.style.marginTop = "-30px"
-  
-      var icon1 = document.getElementById("icon1");
-      icon1.style.display = "none";
-  
-      var button2 = document.getElementById("button2");
-      button2.style.marginTop = "-10px"
-  
-      var heading = document.getElementById("heading");
-      heading.style.marginTop = "0px"
+    var input7 = document.getElementById("input7");
+    input7.setAttribute("type", "password");
+
+    var icon2 = document.getElementById("icon2");
+    icon2.style.display = "block";
+    icon2.style.marginLeft = "320px"
+    icon2.style.marginTop = "-30px"
+
+    var icon1 = document.getElementById("icon1");
+    icon1.style.display = "none";
+
+    var button2 = document.getElementById("button2");
+    button2.style.marginTop = "-10px"
+
+    var heading = document.getElementById("heading");
+    heading.style.marginTop = "0px"
 }
 
-function signUp(){
-        var input2 = document.getElementById("input2");
-        input2.value = " ";
-    
-        var input3 = document.getElementById("input3");
-        input3.value = " ";
+function signUp() {
 
-        var input4 = document.getElementById("input4");
-        input4.value = " ";
-    
-        var input5 = document.getElementById("input5");
-        input5.value = " ";
+    var input4 = document.getElementById("input4");
+    if (input4.value.length === 0) {
+        swal("Please Enter Your First Name");
+    }
+    input4.value = " ";
+
+    var input5 = document.getElementById("input5");
+    if (input5.value.length === 0) {
+        swal("Please Enter Your Last Name");
+    }
+    input5.value = " ";
+
+    var input6 = document.getElementById("input6");
+    if (input6.value.length === 0) {
+        swal("Please Enter Your Email Address");
+    }
+    input6.value = " ";
+
+    var input7 = document.getElementById("input7");
+    if (input7.value.length === 0) {
+        swal("Please Enter Your Password");
+    }
+    else if (input7.value.length < 8) {
+        swal("Password Should Be Must 8 Chracter")
+    }
+    input7.value = " ";
 }
