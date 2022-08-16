dcc8c277db41a2b55c2817ff1674a41e96acdfa7
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-08-16 11:53:36 +0500

    updated

--- app.js
+++ app.js
@@ -2,19 +2,10 @@
 function login() {
     // email address in sign-in-form
     var input2 = document.getElementById("input2");
-    // if (input2.value.length === 0) {
-    //     swal("Please Enter Your Email Address")
-    // }
     input2.value = " ";
 
     // password in sign-in form
     var input3 = document.getElementById("input3");
-    // if (input3.value.length === 0) {
-    //     swal("Please Enter Your Password");
-    // }
-    // else if (input3.value.length < 8) {
-    //     swal("Password Should Be Must 8 Chracter");
-    // }
     input3.value = " ";
 
     //loader
@@ -127,30 +118,15 @@ function icon3() {
 function signUp() {
 
     var input4 = document.getElementById("input4");
-    // if (input4.value.length === 0) {
-    //     swal("Please Enter Your First Name");
-    // }
     input4.value = " ";
 
     var input5 = document.getElementById("input5");
-    // if (input5.value.length === 0) {
-    //     swal("Please Enter Your Last Name");
-    // }
     input5.value = " ";
 
     var input6 = document.getElementById("input6");
-    // if (input6.value.length === 0) {
-    //     swal("Please Enter Your Email Address");
-    // }
     input6.value = " ";
 
     var input7 = document.getElementById("input7");
-    // if (input7.value.length === 0) {
-    //     swal("Please Enter Your Password");
-    // }
-    // else if (input7.value.length < 8) {
-    //     swal("Password Should Be Must 8 Chracter")
-    // }
     input7.value = " ";
 
     var main = document.querySelector(".main1");
@@ -469,7 +445,7 @@ function updateCountDown() {
     var seconds = time % 60;
 
     // seconds = seconds < 30 ? '0' + seconds : seconds;
-    seconds = seconds < 30 ? '0' + seconds : seconds;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countDowne.innerHTML = `${minutes}:${seconds}`;
     time--;
@@ -477,207 +453,5 @@ function updateCountDown() {
 
 
 
-// cssallquizes
-var cssQuizQuestion = [
-    {
-        question: "Q1 : What does CSS stand for?",
-        a: "Creative Style Sheet",
-        b: "Cascading Style Sheet",
-        c: "Computer Style Sheet",
-        d: "Colorful Style Sheet",
-        ans: "anss2",
-    },
-    {
-        question: "Q2 : What is the correct HTML for referring to an external style sheet?",
-        a: "<'stylesheet>mystyle.css</stylesheet'>",
-        b: "<'link rel='stylesheet' type='text/css' href='mystyle.css''>",
-        c: "<'style src='mystyle.css'>",
-        d: "<'style>mystyle.css</style'>",
-        ans: "anss2",
-    },
-    {
-        question: "Q3 : Where in an HTML document is the correct place to refer to an external style sheet?",
-        a: "In the <'body'> section",
-        b: "In the <'head'> section",
-        c: "At the end of Document",
-        d: "<'body'> and <'head'> section",
-        ans: "anss2",
-    },
-    {
-        question: "Q4 : Which HTML tag is used to define an internal style sheet?",
-        a: "<'style'>",
-        b: "<'script'>",
-        c: "<'css'>",
-        d: "<'head'>",
-        ans: "anss1",
-    },
-    {
-        question: "Q5 : Which HTML attribute is used to define inline styles?",
-        a: "styles",
-        b: "class",
-        c: "font",
-        d: "style",
-        ans: "anss4",
-    },
-    {
-        question: "Q6 : Which is the correct CSS syntax?",
-        a: "{body;color:black;}",
-        b: "body {color:black;}",
-        c: "{body:color=black;}",
-        d: "body:color:black;",
-        ans: "anss2",
-    },
-    {
-        question: "Q7 : How do you insert a comment in a CSS file?",
-        a: "// this is comment",
-        b: "/* this is a commment */",
-        c: "// this is a comment //",
-        d: "'this is a comment",
-        ans: "anss2",
-    },
-    {
-        question: "Q8 : Which property is used to change the background color?",
-        a: "background-color",
-        b: "bgcolor",
-        c: "color",
-        d: "backgrounf",
-        ans: "anss1",
-    },
-    {
-        question: "Q9 : How do you add a background color for all <h1> elements?",
-        a: "h1.all {background-color:white;}",
-        b: "all.h1 {background-color:white;}",
-        c: "h1 {background-color:white;}",
-        d: "h1.background-color:white",
-        ans: "anss3",
-    },
-    {
-        question: "Q10 : Which CSS property is used to change the text color of an element?",
-        a: "fgcolor",
-        b: "text-color",
-        c: "color",
-        d: "font-color",
-        ans: "anss3",
-    },
-    {
-        question: "Q11 : Which CSS property controls the text size?",
-        a: "text-size",
-        b: "font-style",
-        c: "text-style",
-        d: "font-size",
-        ans: "anss4",
-    },
-    {
-        question: "Q12 : What is the correct CSS syntax for making all the <p> elements bold?",
-        a: "p {text-size:bold;}",
-        b: "<'p style='font-size:bold;''>",
-        c: "p {font-weight:bold;}",
-        d: "<'p style='text-size:bold;''>",
-        ans: "anss3",
-    },
-    {
-        question: "Q13 : How do you display hyperlinks without an underline?",
-        a: "a {text-decoration:none;}",
-        b: "a {underline:none;}",
-        c: "a {text-decoration:no-underline;}",
-        d: "a {decoration:no-underline;}",
-        ans: "anss1",
-    },
-    {
-        question: "Q14 : How do you make each word in a text start with a capital letter?",
-        a: "text-transform:capitalize",
-        b: "transform:capitalize",
-        c: "you can't do that with css",
-        d: "text-style:capitalize",
-        ans: "anss1",
-    },
-    {
-        question: "Q15 : Which property is used to change the font of an element?",
-        a: "font-weight",
-        b: "font-family",
-        c: "font-style",
-        d: "font-size",
-        ans: "anss2",
-    },
-    {
-        question: "Q16 : How do you make the text bold?",
-        a: "style:bold;",
-        b: "font:bold;",
-        c: "font-weight:bold;",
-        d: "font-style:bold",
-        ans: "anss3",
-    },
-    {
-        question: "Q17 : Which property is used to change the left margin of an element?",
-        a: "padding-left",
-        b: "margin-left",
-        c: "indent",
-        d: "left-margin",
-        ans: "anss2",
-    },
-    {
-        question: "Q18 : When using the padding property; are you allowed to use negative values?",
-        a: "No",
-        b: "Yes",
-        c: "negative",
-        d: "value",
-        ans: "anss1",
-    },
-    {
-        question: "Q19 : How do you make a list that lists its items with squares?",
-        a: "list-style-type: square;",
-        b: "list-type: square;",
-        c: "list: square;",
-        d: "list-style: square;",
-        ans: "anss1",
-    },
-    {
-        question: "Q20 : How do you select an element with id 'demo'?",
-        a: ".demo",
-        b: "*demo",
-        c: "#demo",
-        d: "demo",
-        ans: "anss3",
-    },
-    {
-        question: "Q21 : How do you select elements with class name 'test'?",
-        a: ".test",
-        b: "#test",
-        c: "*test",
-        d: "test",
-        ans: "anss1",
-    },
-    {
-        question: "Q22 : How do you select all p elements inside a div element?",
-        a: "div + p",
-        b: "div p",
-        c: "div.p",
-        d: "div.all.p",
-        ans: "anss2",
-    },
-    {
-        question: "Q23 : How do you group selectors?",
-        a: "Separate each selecter with a comma",
-        b: "Separate each selecter with a space",
-        c: "Separate each selecter with a plus sign",
-        d: "Separate each selecter with a dot",
-        ans: "anss1",
-    },
-    {
-        question: "Q24 : What is the default value of the position property?",
-        a: "fixed",
-        b: "relative",
-        c: "absolute",
-        d: "static",
-        ans: "anss4",
-    },
-    {
-        question: "Q25 : Which property is used to change the left padding of an element?",
-        a: "padding-left",
-        b: "margin-left",
-        c: "indent",
-        d: "left-padding",
-        ans: "anss1",
-    },
-]
+
 
